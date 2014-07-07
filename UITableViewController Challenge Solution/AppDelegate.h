cbaf9b5a604812957a5dd96f531bca5753b351a9
Mike McNamara <Mike@Mikes-MacBook-Air.local>
2014-07-07 12:04:31 +0100

    Initial Commit

---
+++ UITableViewController Challenge Solution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Mike McNamara on 07/07/2014.
+//  Copyright (c) 2014 Mike McNamara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableViewController Challenge Solution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Mike McNamara on 07/07/2014.
+//  Copyright (c) 2014 Mike McNamara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
